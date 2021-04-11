4b8d88b31ac2b46f5756ad2eeacf357146102699
Shashant-R <62503767+Shashant-R@users.noreply.github.com>
2021-04-11 16:04:10 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,11 @@
 import React from 'react';
 //import './Card.css';
-const Card =({id, username, email}) =>{
+const Card =({id, name, email}) =>{
 	return (
 		<div className='tc bg-light-red dib br3 pa3 ma2 grow bw2 shadow-5' id='flip-vertical-left'>
 			<img alt='monster' src={`https://robohash.org/${99*id+1}}?set=set2&size=200x200`} />
 			<div>
-				<h2>{username}</h2>
+				<h2>{name}</h2>
 				<p>{email}</p>
 			</div>
 		</div>
