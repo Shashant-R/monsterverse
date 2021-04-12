197d6ca05a162b5c0f3180835988ce10bac0fd06
Shashant-R <shashant7699@gmail.com>
2021-04-12 20:45:28 +0530

    using hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import Scroll from '../components/Scroll';
 import SearchBox from '../components/SearchBox';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
-class App extends Component {
-	constructor() {
+function App(){
+	/*constructor() {
 		super()
 		this.state = {
 			Monsters: [],
 			searchField: ''
 		}
-	}
+	}*/
+	const [Monsters, setMonsters] = useState([]);
+	const [searchField, setSearchField] = useState('');
+	useEffect(()=>{
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response=> {
+			return response.json();
+		})
+		.then(users=> {
+			setMonsters(users);
+		});
+	},[])
+	/*
 	componentDidMount() {
 		fetch('https://jsonplaceholder.typicode.com/users')
 		.then(response=> {
@@ -21,28 +33,28 @@ class App extends Component {
 		.then(users=> {
 			this.setState({ Monsters: users})
 		});
+	}*/
+	const onSearchChange = (event) => {
+		setSearchField(event.target.value);
+		//this.setState({searchField : event.target.value});
 	}
-	onSearchChange = (event) => {
-		this.setState({searchField : event.target.value});
-	}
-	render (){
-		const {Monsters, searchField} = this.state;
-		const filteredMonsters =Monsters.filter(monster => {
-			return monster.name.toLowerCase().includes(searchField.toLowerCase());
-		})
-		return !Monsters.length ? 
-			<h2 className='tc white'>Monsters approaching ...... </h2> :
-			(
-				<div className='tc'>
-					<h1 className='f1'>Monsterverse</h1>
-					<SearchBox searchChange={this.onSearchChange}/>
-					<Scroll>
-						<ErrorBoundary>
-							<CardList Monsters={filteredMonsters}/>
-						</ErrorBoundary>
-					</Scroll>
-				</div>
-			);	
-	}
+	
+	//const {Monsters, searchField} = this.state;
+	const filteredMonsters =Monsters.filter(monster => {
+		return monster.name.toLowerCase().includes(searchField.toLowerCase());
+	})
+	return !Monsters.length ? 
+		<h2 className='tc white'>Monsters approaching ...... </h2> :
+		(
+			<div className='tc'>
+				<h1 className='f1'>Monsterverse</h1>
+				<SearchBox searchChange={onSearchChange}/>
+				<Scroll>
+					<ErrorBoundary>
+						<CardList Monsters={filteredMonsters}/>
+					</ErrorBoundary>
+				</Scroll>
+			</div>
+		);	
 }
 export default App;
